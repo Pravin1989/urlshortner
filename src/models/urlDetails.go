8501b166f36099c59d1f1250a01acffe12482cef
Pravin1989 <Pravin.Budge@gmail.com>
2021-12-27 18:45:23 +0530

    Added implementation to encode the URL using sha256 hash algorithm

--- src/models/urlDetails.go
+++ src/models/urlDetails.go
@@ -1,16 +1,31 @@
 package models
 
 import (
+	"crypto/sha1"
+	"encoding/base64"
+
 	"github.com/urlshortner/src/entity"
 )
 
-var urlsMap = make(map[int]interface{})
+const (
+	baseURL = "http://urlshortner.com/"
+)
+
+var urlsMap = make(map[string]string)
 
 func EncodeUrlAndStore(input entity.URL) string {
-	value, isExist := urlsMap[len(input.URL)]
+	value, isExist := urlsMap[input.URL]
 	if isExist {
-		return value.(string)
+		return value
 	}
-	urlsMap[len(input.URL)] = input.URL
-	return input.URL
+	encodedValueToStore := Encode(input)
+	urlsMap[encodedValueToStore] = input.URL
+	return baseURL + encodedValueToStore
+}
+
+func Encode(input entity.URL) string {
+	hasher := sha1.New()
+	hasher.Write([]byte(input.URL))
+	encodedValue := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+	return encodedValue[0:6]
 }
